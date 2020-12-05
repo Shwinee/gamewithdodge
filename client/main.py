ee652694d082402b0048de8aa142c80853706439
dodgech <robertd.ch@icloud.com>
2020-12-05 17:55:31 -0500

    first python file - client - dodge

---
+++ client/main.py
@@ -0,0 +1,17 @@
+import random
+import time
+
+thing = 10
+thing = thing+1
+
+
+while(thing > 1):
+    thingh = (random.randint(1, 3))
+    if(thingh == 1):
+        print("ni")
+    if (thingh == 2):
+        print("titties")
+    if (thingh == 3):
+        print("jacob do your parents beat you")
+    time.sleep(5)
+    thing = thing-1
