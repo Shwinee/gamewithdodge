b41c5bc252e9b7ac75a4f0adf0df1da06de407df
dodgech <robertd.ch@icloud.com>
2020-12-05 20:52:01 -0500

    made basic game - client - dodge

--- client/main.py
+++ client/main.py
@@ -7,3 +7,57 @@ correct_payload = {'i': i}
 
 r = requests.post(url, data=correct_payload)
 print(r.text)
+
+import turtle
+
+#screen
+wn = turtle.Screen()
+wn.bgcolor("white")
+player = turtle.Turtle()
+player.color("black")
+player.shape("triangle")
+speed = 0
+player.penup()
+turtle.listen()
+
+#borders
+mypen = turtle.Turtle()
+mypen.penup()
+mypen.setposition(-175, -300)
+mypen.pendown()
+mypen.pensize(3)
+for side in range(8):
+    mypen.forward(250)
+    mypen.left(45)
+mypen.hideturtle()
+
+mypen = turtle.Turtle()
+mypen.penup()
+mypen.setposition(-125, -175)
+mypen.pendown()
+mypen.pensize(3)
+for side in range(8):
+    mypen.forward(150)
+    mypen.left(45)
+mypen.hideturtle()
+
+#definitions
+def turnleft():
+    player.left(15)
+def turnright():
+    player.right(15)
+def increasespeed():
+    global speed
+    speed += 1
+def decreasespeed():
+    global speed
+    speed-= 1
+
+#controls
+turtle.onkey(turnleft, "Left")
+turtle.onkey(turnright, "Right")
+turtle.onkey(increasespeed, "Up")
+turtle.onkey(decreasespeed, "Down")
+#speed
+while True:
+    player.forward(speed)
